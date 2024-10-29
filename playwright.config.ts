bee1d418df0aa8f8b84fd2274a2ad7e07bdb9ccf
perminovea <perminov09@gmail.com>
2024-10-29 21:47:29 +0500

    add 11

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,18 +24,18 @@ export default defineConfig({
   //forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  // reporter: [
-  //   ["list", { printSteps: true }],
-  //   ["html", { open: "never", outputFolder: "./test-results/report/" }],
-  //   [
-  //     "junit",
-  //     { outputFolder: "./test-results/report/", outputFile: "results.xml" },
-  //   ],
-  // ],
+  reporter: [
+    ["list", { printSteps: true }],
+    ["html", { open: "never", outputFolder: "./test-results/report/" }],
+    [
+      "junit",
+      { outputFolder: "./test-results/report/", outputFile: "results.xml" },
+    ],
+  ],
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "dot" : "list",
+  //reporter: process.env.CI ? "dot" : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "https://magento.softwaretestingboard.com",
